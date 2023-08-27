c5e8c1c1bc8b545b53225e213cf6fad8d2a892d7
iber <iberson-silva@outlook.com>
2023-08-27 16:20:51 -0500

    agregué dos componentes: card y slider con swiper y angular material

---
+++ Biblioteca/src/app/components/slider/slider.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.css'],
+})
+export class SliderComponent {
+}
