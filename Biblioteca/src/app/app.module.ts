314dd1bbe217775dddf2a1cc96b253cebb355464
iber <iberson-silva@outlook.com>
2023-08-28 02:29:41 -0500

    implementacion del componente list

--- Biblioteca/src/app/app.module.ts
+++ Biblioteca/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatButtonModule} from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
@@ -9,12 +9,14 @@ import { ReserveComponent } from './reserve/reserve.component';
 import { HomeComponent } from './home/home.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SliderComponent } from './components/slider/slider.component';
-// import function to register Swiper custom elements
 import { register } from 'swiper/element/bundle';
-// register Swiper custom elements
-
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { CardComponent } from './components/card/card.component';
+// import { CardComponent } from './components/card/card.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+
+
 
 register();
 
@@ -27,12 +29,17 @@ register();
     HomeComponent,
     SidebarComponent,
     SliderComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatSlideToggleModule,
-    CardComponent
+    // CardComponent,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent],
